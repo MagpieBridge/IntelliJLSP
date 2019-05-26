d1a910348ec7abd9d18c9b574ed9402feeff2ca9
Julian Dolby <juliandolby@gmail.com>
2019-05-26 18:01:41 -0400

    configuration fixes

--- src/org/magpiebridge/intellij/plugin/Configuration.java
+++ src/org/magpiebridge/intellij/plugin/Configuration.java
@@ -3,11 +3,14 @@ package org.magpiebridge.intellij.plugin;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.ui.components.JBOptionButton;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 public class Configuration implements Configurable {
@@ -46,47 +49,48 @@ public class Configuration implements Configurable {
         p.add(jarText);
 
         JTextField jarField = new JTextField(jarPath, 50);
-        jarField.addActionListener(e -> pc.setValue(JAR, jarField.getText()));
         p.add(jarField);
-
-        p.add(new JLabel("-- OR --"));
 
         JComponent cpText = new JLabel("Java class path");
         p.add(cpText);
 
         JTextField cpField = new JTextField(cpPath, 50);
-        cpField.addActionListener(e -> pc.setValue(CP, cpField.getText()));
         p.add(cpField);
 
         JComponent mainText = new JLabel("Main class name");
         p.add(mainText);
 
         JTextField mainField = new JTextField(mainClass, 50);
-        mainField.addActionListener(e -> pc.setValue(MAIN, mainField.getText()));
         p.add(mainField);
-
-        p.add(new JLabel("--------------"));
 
         JComponent argsText = new JLabel("Java arguments");
         p.add(argsText);
 
         JTextField argsField = new JTextField(args, 50);
-        argsField.addActionListener(e -> pc.setValue(ARGS, argsField.getText()));
         p.add(argsField);
 
         JComponent dirText = new JLabel("Java working directory");
         p.add(dirText);
 
         JTextField dirField = new JTextField(dir, 50);
-        dirField.addActionListener(e -> pc.setValue(DIR, dirField.getText()));
         p.add(dirField);
 
         JComponent jvmText = new JLabel("JVM absolute path");
         p.add(jvmText);
 
         JTextField jvmField = new JTextField(jvm, 50);
-        jvmField.addActionListener(e -> pc.setValue(JVM, jvmField.getText()));
         p.add(jvmField);
+
+        JButton set = new JButton("set configuration");
+        set.addActionListener(e -> {
+            pc.setValue(JAR, jarField.getText());
+            pc.setValue(CP, cpField.getText());
+            pc.setValue(MAIN, mainField.getText());
+            pc.setValue(ARGS, argsField.getText());
+            pc.setValue(DIR, dirField.getText());
+            pc.setValue(JVM, jvmField.getText());
+        });
+        p.add(set);
 
         return p;
     }
