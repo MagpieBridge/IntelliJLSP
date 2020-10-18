62bf6586842e14579b5b55446948e3ad10970804
Markus Schmidt <markus262@web.de>
2020-10-18 17:33:14 +0200

    wip:playground: custom problems/diagnostics toolwindow

--- src/main/java/magpiebridge/intellij/plugin/ServerLauncher.java
+++ src/main/java/magpiebridge/intellij/plugin/ServerLauncher.java
@@ -1,18 +1,33 @@
 package magpiebridge.intellij.plugin;
 
 import com.google.common.collect.Lists;
+import com.intellij.analysis.problemsView.toolWindow.ProblemsView;
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.errorTreeView.*;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.CustomizeColoredTreeCellRenderer;
+import com.intellij.ui.SimpleColoredComponent;
+import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
+import lsp4intellij.LSPNavigatable;
 import lsp4intellij.MyRawCommandServerDefinition;
 import lsp4intellij.SocketServerDefinition;
+import org.eclipse.lsp4j.*;
+import org.jetbrains.annotations.NotNull;
 import org.wso2.lsp4intellij.IntellijLanguageClient;
 import org.wso2.lsp4intellij.client.languageserver.serverdefinition.LanguageServerDefinition;
+import org.wso2.lsp4intellij.utils.FileUtils;
 
 import javax.annotation.Nullable;
+import javax.swing.*;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -119,6 +134,172 @@ public class ServerLauncher {
     service.setConfigParams(Lists.newArrayList(map.entrySet()));
     service.init();
     service.initProjectConnections(project);
+
+
+    // TESTING AREA
+
+    final Diagnostic diagnostic = new Diagnostic();
+    diagnostic.setMessage("This is a Jimple Error. ");
+    diagnostic.setRange(new Range(new Position(2, 0), new Position(2, 10)));
+    diagnostic.setSource("Jimpleparser");
+    diagnostic.setSeverity(DiagnosticSeverity.Error);
+
+    List<DiagnosticRelatedInformation> related = new ArrayList<>();
+    related.add(new DiagnosticRelatedInformation(new Location("file:///home/smarkus/IdeaProjects/JimpleLspExampleProject/module1/src/invalid.jimple", new Range(new Position(4, 4), new Position(4, 10))), "problem A"));
+    related.add(new DiagnosticRelatedInformation(new Location("file:///home/smarkus/IdeaProjects/JimpleLspExampleProject/module1/src/invalid.jimple", new Range(new Position(6, 6), new Position(6, 12))), "problem B"));
+    related.add(new DiagnosticRelatedInformation(new Location("file:///home/smarkus/IdeaProjects/JimpleLspExampleProject/module1/src/invalid.jimple", new Range(new Position(8, 8), new Position(8, 14))), "problem C"));
+    diagnostic.setRelatedInformation(related);
+
+
+    ApplicationManager.getApplication().invokeLater(() -> {
+      final ContentManager contentManager = ProblemsView.getToolWindow(project).getContentManager();
+      final Content selectedContent = contentManager.getSelectedContent();
+
+      final LSPProblemsViewPanel component = new LSPProblemsViewPanel(project);
+      // TODO: add all diagnostics of that file
+      component.addDiagnostic(diagnostic, "file:///home/smarkus/IdeaProjects/JimpleLspExampleProject/module1/src/helloworld.jimple");
+      component.expandAll();
+
+      final Content content = contentManager.getFactory().createContent(component, "Diagnostics", false);
+      selectedContent.getManager().addContent(content);
+      selectedContent.getManager().setSelectedContent(content, true, true);
+
+    });
+
+  }
+
+
+  public static final class LSPProblemsViewPanel extends NewErrorTreeViewPanel {
+    public LSPProblemsViewPanel(Project project) {
+      super(project, null);
+    }
+
+    @Override
+    protected boolean canHideWarnings() {
+      return false;
+    }
+
+
+    public void addDiagnostic(Diagnostic diagnostic, String fileUri) {
+      final VirtualFile vf = FileUtils.virtualFileFromURI(fileUri);
+      final ErrorViewStructure errorViewStructure = getErrorViewStructure();
+
+      final GroupingElement groupingElement = new DiagGroupingElement(diagnostic.getMessage(), diagnostic, vf);
+
+      final List<DiagnosticRelatedInformation> diagnosticRelatedInformations = diagnostic.getRelatedInformation();
+      if (diagnosticRelatedInformations != null && !diagnosticRelatedInformations.isEmpty()) {
+        for (DiagnosticRelatedInformation relatedInformation : diagnosticRelatedInformations) {
+          errorViewStructure.addNavigatableMessage(diagnostic.getMessage(), new LSPNavigatableMessageElement(relatedInformation, groupingElement, myProject));
+        }
+
+
+      }
+
+    }
+
+
+  }
+
+  private static class LSPNavigatableMessageElement extends NavigatableMessageElement {
+/*
+    public LSPNavigatableMessageElement(Diagnostic diagnostic, GroupingElement group, String uri, Project project) {
+      super(getType(diagnostic.getSeverity()), group, new String[]{diagnostic.getMessage()}, new LSPNavigatable(project, uri, diagnostic.getRange()), "", "");
+    }
+    */
+
+    public LSPNavigatableMessageElement(DiagnosticRelatedInformation relatedInfo, GroupingElement group, Project project) {
+      super(group.getKind(), group, new String[]{relatedInfo.getMessage(), "new line 1", "new two"}, new LSPNavigatable(project, relatedInfo.getLocation()), "", "");
+    }
+
+    /*
+    private static ErrorTreeElementKind getType(DiagnosticSeverity severity) {
+      return ErrorTreeElementKind.ERROR;
+    }
+    */
+
+    @Override
+    public @org.jetbrains.annotations.Nullable CustomizeColoredTreeCellRenderer getLeftSelfRenderer() {
+      return new CustomizeColoredTreeCellRenderer() {
+        @Override
+        public void customizeCellRenderer(SimpleColoredComponent renderer,
+                                          JTree tree,
+                                          Object value,
+                                          boolean selected,
+                                          boolean expanded,
+                                          boolean leaf,
+                                          int row,
+                                          boolean hasFocus) {
+          renderer.setIcon(AllIcons.General.Error);
+
+          final String[] text = getText();
+          if (text != null) {
+            // FIXME: render multiple lines
+            renderer.append(text[0], SimpleTextAttributes.REGULAR_ATTRIBUTES);
+          }
+          renderer.append(" ");
+          renderer.append(FileUtils.shortenFileUri("bka/bla/helloworld.jimple:12:34"), SimpleTextAttributes.GRAYED_ATTRIBUTES);
+        }
+      };
+    }
+
+    @Override
+    public @NotNull String getPresentableText() {
+      return "";
+    }
+
+  }
+
+
+  private static final class DiagGroupingElement extends GroupingElement {
+
+    public DiagGroupingElement(String name, Object data, VirtualFile file) {
+      super(name, data, file);
+      setKind(ErrorTreeElementKind.ERROR); // TODO
+    }
+
+    @Override
+    public @org.jetbrains.annotations.Nullable CustomizeColoredTreeCellRenderer getLeftSelfRenderer() {
+      return new CustomizeColoredTreeCellRenderer() {
+        @Override
+        public void customizeCellRenderer(SimpleColoredComponent renderer,
+                                          JTree tree,
+                                          Object value,
+                                          boolean selected,
+                                          boolean expanded,
+                                          boolean leaf,
+                                          int row,
+                                          boolean hasFocus) {
+          renderer.setIcon(AllIcons.General.Error);
+
+          final String[] text = getText();
+          if (text != null) {
+            // FIXME: render multiple lines
+            renderer.append(text[0], SimpleTextAttributes.REGULAR_ATTRIBUTES);
+          }
+          renderer.append(" ");
+          renderer.append(FileUtils.shortenFileUri("bka/bla/helloworld.jimple:12:34"), SimpleTextAttributes.GRAYED_ATTRIBUTES);
+        }
+      };
+    }
+
+    @Override
+    public @org.jetbrains.annotations.Nullable CustomizeColoredTreeCellRenderer getRightSelfRenderer() {
+      return new CustomizeColoredTreeCellRenderer() {
+        @Override
+        public void customizeCellRenderer(SimpleColoredComponent renderer,
+                                          JTree tree,
+                                          Object value,
+                                          boolean selected,
+                                          boolean expanded,
+                                          boolean leaf,
+                                          int row,
+                                          boolean hasFocus) {
+
+          // FIXME -> getSource
+          renderer.append("JimpleParser", SimpleTextAttributes.GRAYED_ATTRIBUTES);
+        }
+      };
+    }
   }
 
 
